7c4ededc24a6b9924c301572e1a98281cc4396ab
root <nathanporter823@gmail.com>
2015-12-22 00:19:11 -0800

    Added interface and hostapd configuration

--- apAutoConfig.sh
+++ apAutoConfig.sh
@@ -6,13 +6,13 @@
 #anonomizing access point. 
 
 
-#sudo ifdown wlan0
+sudo ifdown wlan0
 
 echo "Updating system software..."
-#sudo apt-get update 
+sudo apt-get update 
 
 echo "Installing hostapd and the dhcp server..."
-#sudo apt-get install hostapd isc-dhcp-server sed
+sudo apt-get install hostapd isc-dhcp-server sed
 
 
 #Test internet connection
@@ -23,27 +23,27 @@ host2=wikipedia.org
 #((ping -w5 -c3 $host1 || ping -w5 -c3 $host2) > /dev/null 2>&1) && echo "Internet connectivity - OK" || (echo "Internet connectivity - Down, Internet connectivity is required for this script to complete. exiting..." && exit 1)
 
 echo "Updating system software..."
-#sudo apt-get update 
+sudo apt-get update 
 
 echo "Installing hostapd and the dhcp server..."
-#sudo apt-get install hostapd isc-dhcp-server
+sudo apt-get install hostapd isc-dhcp-server
 
 ########################
 ##Configure dhcpd.conf##
 ########################
-#sudo sed -i 's/option domain-name "example.org";/#option domain-name "example.org";/' /etc/dhcp/dhcpd.conf
-#sudo sed -i 's/option domain-name-servers ns1.example.org, ns2.example.org;/#option domain-name-servers ns1.example.org, ns2.example.org;/' /etc/dhcp/dhcpd.conf
-#sudo sed -i 's/#authoritative;/authoritative;/' /etc/dhcp/dhcpd.conf
+sudo sed -i 's/option domain-name "example.org";/#option domain-name "example.org";/' /etc/dhcp/dhcpd.conf
+sudo sed -i 's/option domain-name-servers ns1.example.org, ns2.example.org;/#option domain-name-servers ns1.example.org, ns2.example.org;/' /etc/dhcp/dhcpd.conf
+sudo sed -i 's/#authoritative;/authoritative;/' /etc/dhcp/dhcpd.conf
 
-#sudo sed -i "\$asubnet 192.168.42.0 netmask 255.255.255.0 {" /etc/dhcp/dhcpd.conf
-#sudo sed -i "\$arange 192.168.42.10 192.168.42.50;" /etc/dhcp/dhcpd.conf
-#sudo sed -i "\$aoption broadcast-address 192.168.42.255;" /etc/dhcp/dhcpd.conf
-#sudo sed -i "\$aoption routers 192.168.42.1;" /etc/dhcp/dhcpd.conf
-#sudo sed -i "\$adefault-lease-time 600;" /etc/dhcp/dhcpd.conf
-#sudo sed -i "\$amax-lease-time 7200;" /etc/dhcp/dhcpd.conf
-#sudo sed -i "\$aoption domain-name "local";" /etc/dhcp/dhcpd.conf
-#sudo sed -i "\$aoption domain-name-servers 8.8.8.8, 8.8.4.4;" /etc/dhcp/dhcpd.conf
-#sudo sed -i "\$a}" /etc/dhcp/dhcpd.conf
+sudo sed -i "\$asubnet 192.168.42.0 netmask 255.255.255.0 {" /etc/dhcp/dhcpd.conf
+sudo sed -i "\$arange 192.168.42.10 192.168.42.50;" /etc/dhcp/dhcpd.conf
+sudo sed -i "\$aoption broadcast-address 192.168.42.255;" /etc/dhcp/dhcpd.conf
+sudo sed -i "\$aoption routers 192.168.42.1;" /etc/dhcp/dhcpd.conf
+sudo sed -i "\$adefault-lease-time 600;" /etc/dhcp/dhcpd.conf
+sudo sed -i "\$amax-lease-time 7200;" /etc/dhcp/dhcpd.conf
+sudo sed -i "\$aoption domain-name "local";" /etc/dhcp/dhcpd.conf
+sudo sed -i "\$aoption domain-name-servers 8.8.8.8, 8.8.4.4;" /etc/dhcp/dhcpd.conf
+sudo sed -i "\$a}" /etc/dhcp/dhcpd.conf
 
 ########################
 ##Configure Interfaces##
@@ -56,7 +56,7 @@ sudo sed -i -e "\$aaddress 192.168.42.1" /etc/network/interfaces
 sudo sed -i -e "\$anetmask 255.255.255.0" /etc/network/interfaces
 sudo sed -i -e "\$aup iptables-restore < /etc/iptables.ipv4.nat" /etc/network/interfaces
 
-#sudo ifconfig wlan0 192.168.42.1
+sudo ifconfig wlan0 192.168.42.1
 
 echo > /etc/hostapd/hostapd.conf
 sudo sed -i -e "1i interface=wlan0" /etc/hostapd/hostapd.conf
