f3ed2cbbe1e663bbabb2d8e5dccb4178d63cdbc5
root <nathanporter823@gmail.com>
2015-12-22 09:58:14 -0800

    Fixed small errors

--- apAutoConfig.sh
+++ apAutoConfig.sh
@@ -22,15 +22,10 @@ host2=wikipedia.org
 #Uncomment before running. The nested case statment breaks Sublime Text's syntax highlighter.
 #((ping -w5 -c3 $host1 || ping -w5 -c3 $host2) > /dev/null 2>&1) && echo "Internet connectivity - OK" || (echo "Internet connectivity - Down, Internet connectivity is required for this script to complete. exiting..." && exit 1)
 
-echo "Updating system software..."
-sudo apt-get update 
 
-echo "Installing hostapd and the dhcp server..."
-sudo apt-get install hostapd isc-dhcp-server
-
-########################
-##Configure dhcpd.conf##
-########################
+###############################
+## Configure the DHCP Server ##
+###############################
 sudo sed -i 's/option domain-name "example.org";/#option domain-name "example.org";/' /etc/dhcp/dhcpd.conf
 sudo sed -i 's/option domain-name-servers ns1.example.org, ns2.example.org;/#option domain-name-servers ns1.example.org, ns2.example.org;/' /etc/dhcp/dhcpd.conf
 sudo sed -i 's/#authoritative;/authoritative;/' /etc/dhcp/dhcpd.conf
@@ -45,9 +40,10 @@ sudo sed -i "\$aoption domain-name "local";" /etc/dhcp/dhcpd.conf
 sudo sed -i "\$aoption domain-name-servers 8.8.8.8, 8.8.4.4;" /etc/dhcp/dhcpd.conf
 sudo sed -i "\$a}" /etc/dhcp/dhcpd.conf
 
-########################
-##Configure Interfaces##
-########################
+
+##########################
+## Configure Interfaces ##
+##########################
 sudo sed -i 's/INTERFACES=""/INTERFACES="wlan0"/' /etc/default/isc-dhcp-server
 
 sudo sed -i -e "\$aallow-hotplug wlan0" /etc/network/interfaces
