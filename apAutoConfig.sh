38baadfe1e1fe3ccee51cfc8e8cf33016b33ed3f
eepromnk <nathanporter823@gmail.com>
2016-01-18 23:30:21 -0800

    Made a small change

--- apAutoConfig.sh
+++ apAutoConfig.sh
@@ -15,14 +15,6 @@ echo "Installing hostapd and the dhcp server..."
 sudo apt-get --yes install hostapd isc-dhcp-server sed
 
 
-#Test internet connection
-host1=google.com
-host2=wikipedia.org
-
-
-((ping -w5 -c3 $host1 || ping -w5 -c3 $host2) > /dev/null 2>&1) && echo "Internet connectivity - OK" || (echo "Internet connectivity - Down, Internet connectivity is required for this script to complete. exiting..." && exit 1)
-
-
 ###############################
 ## Configure the DHCP Server ##
 ###############################
